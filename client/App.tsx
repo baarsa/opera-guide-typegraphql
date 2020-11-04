de634e8efdd5fbf7e9c8a024580359ef64711af8
U_M13BV <kharchevbv@gmail.com>
2020-11-04 20:28:10 +0300

    put navigation into app

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,40 @@
 import * as React from 'react';
-import { ApolloProvider } from "@apollo/react-hooks";
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import ReactNotification, { store } from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
-import { client } from './apollo-client-setup';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
 import {PerformanceSubscriber} from "./components/performance-subscriber/performance-subscriber";
 import {Operas} from "./pages/operas/operas";
+import {Navigation} from "./components/navigation/navigation";
 
-export const App = () =>
-    <ApolloProvider client={client}>
-        <ReactNotification />
-        <PerformanceSubscriber />
-        <BrowserRouter>
-            <Switch>
-                <Route path='/operas' component={Operas} />
-                <Redirect to='/operas' />
-            </Switch>
-        </BrowserRouter>
-    </ApolloProvider>
+const navigationItems = [
+    {
+        text: 'Opera List',
+        link: '/operas',
+        isActive: false,
+    },
+    {
+        text: 'Add Opera',
+        link: '/operas/create',
+        isActive: false,
+    },
+];
+
+export const App = () => {
+    const location = useLocation();
+    const actualNavigationItems = navigationItems.map(item => ({
+        ...item,
+        isActive: location.pathname === item.link,
+    }));
+        return (
+            <>
+            <ReactNotification/>
+            <PerformanceSubscriber/>
+                <Navigation items={ actualNavigationItems }/>
+                <Switch>
+                    <Route path='/operas' component={Operas}/>
+                    <Redirect to='/operas'/>
+                </Switch>
+            </>
+        );
+    }
 ;
--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from "./App";
+import {client} from "./apollo-client-setup";
+import {ApolloProvider} from "@apollo/react-hooks";
+import {BrowserRouter} from "react-router-dom";
 
-render(<App />, document.getElementById('root'));
+render(
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>,
+    document.getElementById('root'));
